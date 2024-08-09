50f8a4b13e97dbc6649144d260115aa7f0aa79b9
Shivam Rajput <98.shivrajput@gmail.com>
2024-08-09 17:23:55 +0530

    gc_add_rem

--- 1_STUDENT_PROFILE.py
+++ 1_STUDENT_PROFILE.py
@@ -12,7 +12,6 @@ from streamlit_extras.let_it_rain import rain
 import os
 import datetime
 import statistics
-
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -117,7 +116,7 @@ def sgpacal():
 @st.experimental_fragment
 def find(SUBC: str, TYPE: str, sendsorted=True) -> dict:
 
-    SUBJECT_GROUP_STR = [" CO101 | CO102 | CO116 | CO105 ", ]
+    SUBJECT_GROUP_STR = [" CO101 | CO102 | CO116 | CO105 ",]
 
     for sub_grp in SUBJECT_GROUP_STR:
         if SUBC in sub_grp:
@@ -425,7 +424,6 @@ def studyResources_menu():
     st.markdown("---")
 
     s1, s2, s3, s4 = st.columns(4)
-
     st.markdown("---")
 
     with open("DATA_MAIN1.json", 'r') as fl:
@@ -440,13 +438,13 @@ def studyResources_menu():
         with study_mtitle:
             st.write(f"""
                         <h2 style="text-align: center; align-items: center; padding-top: 5px; padding-botton: 5px
-                        "><span style="color: {color};"></span>{RESOURCE_BRNCH} SEM{RESOURCE_SEM} STUDY MATERIALS</h2>
+                        "><span style="color: {color};"></span>[ {RESOURCE_BRNCH} | SEM{RESOURCE_SEM} ] STUDY MATERIALS</h2>
                         """, unsafe_allow_html=True)
 
         if RESOURCE_BRNCH in DATA[0].keys():
             with s2:
-                RESOURCE_SEM = st.selectbox("CHOOSE SEMESTER: ", DATA[0][RESOURCE_BRNCH].keys(),
-                                            placeholder='SEMEMSTER', index=1)
+                RESOURCE_SEM = st.selectbox("CHOOSE SEMESTER: ", DATA[0][RESOURCE_BRNCH].keys(), placeholder='SEMEMSTER', index=1)
+
             if RESOURCE_SEM:
                 with s3:
                     RESOURCE_SUBJECT = st.selectbox("CHOOSE SUBJECT: ", DATA[0][RESOURCE_BRNCH][RESOURCE_SEM].keys(),
@@ -455,7 +453,7 @@ def studyResources_menu():
                 with study_mtitle:
                     st.write(f"""
                                 <h2 style="text-align: center; align-items: center; padding-top: 5px; padding-botton: 5px
-                                "><span style="color: {color};">| {RESOURCE_BRNCH} | SEM{RESOURCE_SEM} | </span>STUDY MATERIAL</h2>
+                                "><span style="color: {color};">[ {RESOURCE_BRNCH} | SEM{RESOURCE_SEM} ] </span>STUDY MATERIAL</h2>
                                 """, unsafe_allow_html=True)
 
                 if RESOURCE_SUBJECT:
@@ -463,22 +461,28 @@ def studyResources_menu():
                         RESOURCE_TYPE = st.selectbox("CHOOSE TYPE: ",
                                                      ['PYQ', 'NOTES', 'PLAYLISTS', 'ASSIGNMENTS', 'BOOKS'], index=0,
                                                      placeholder='TYPE')
+
                     RESOURCE_SUBCODE = DATA[0][RESOURCE_BRNCH][RESOURCE_SEM][RESOURCE_SUBJECT]
+
                     if RESOURCE_SUBCODE != "NONE":
+
                         with open('DATA_MAIN2.json', 'r') as fl2:
                             DATA2 = json.load(fl2)
 
                         if RESOURCE_TYPE:
                             material = find(RESOURCE_SUBCODE, RESOURCE_TYPE, True)
+
                             if material == False:
+
                                 st.markdown("<br><br>", unsafe_allow_html=True)
                                 st.warning(
                                     f"SORRY :< CURRENTLY WE DON'T HAVE ANY RESOURCES RELATED TO [ {RESOURCE_BRNCH} | SEM{RESOURCE_SEM} | {RESOURCE_SUBJECT} ]")
                                 st.warning(
                                     f"WE ARE WORKING ON ADDING MORE BEST RESOURCES, IT WILL TAKE TIME! WE HAVE JUST STARTED WITH THIS 'RESOURCES' PART.")
-                            else:
-                                st.markdown("<br>", unsafe_allow_html=True)
 
+                            else:
+
+                                st.markdown("<br>", unsafe_allow_html=True)
                                 last_Rest, no_of_row = len(material) % 3, len(material) // 3 + 1
                                 if last_Rest == 0: no_of_row -= 1
 
@@ -551,6 +555,7 @@ def studyResources_menu():
 
 
                     else:
+
                         st.markdown("<br><br>", unsafe_allow_html=True)
                         st.warning(
                             f"SORRY :< CURRENTLY WE DON'T HAVE ANY RESOURCES RELATED TO [ {RESOURCE_BRNCH} | SEM{RESOURCE_SEM} | {RESOURCE_SUBJECT} ]")
@@ -688,107 +693,6 @@ if selected=='PROFILE':
         Mtitle.write(f"""
             <h2 style="text-align: center; align-items: center;"><span style="color: {color};">DTU</span> Student Profile 2026</h2>
             """,unsafe_allow_html=True)
-
-
-
-
-#-----------------------------SPECIAL ADDITION IN THE WEBSITE, KIRTI--------------------------------------------------------------------------------------------------------
-
-    if 'best gc' in result_search_box.lower() or 'sexy gc' in result_search_box.lower() or 'w gc'  in result_search_box.lower():
-        descripE.empty()
-        other = True
-        st.header("OHH YOU ASKED ABOUT THE BEST GC ?")
-        st.subheader("Well lemme tell you....")
-
-        st.write(f"""
-                                    <h6 style="text-align: center; align-items: center;"><span style="color: {color};"></span>Keep Scrolling down as the page loads more lines by itself :)</h6>
-                                    """, unsafe_allow_html=True)
-
-        st.markdown("<br><br><br><br><br><br>", unsafe_allow_html=True)
-
-        st.write(f"""
-                            <h3 style="text-align: center; align-items: center;"><span style="color: {color};"></span>THE BEST GC AWARD GOES TO !!!???</h3>
-                            """, unsafe_allow_html=True)
-
-        st.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
-        time.sleep(2)
-
-        st.write(f"""
-                                    <h1 style="text-align: center; align-items: center;"><span style="color: {color};">THE GROUP SEXTING</span></h1>
-                                    """, unsafe_allow_html=True)
-
-        time.sleep(4)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-        st.write(f"""
-                    <h3 style="text-align: center; align-items: center;">REAL BKCHOD FUN<span style="color: {color};"> GC </span> HAI! ISME VO VIBE TO HAI! FR.</h3>
-                    """, unsafe_allow_html=True)
-
-        time.sleep(4.5)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-        st.columns([0.3,1,0.3])[1].write(f"""
-                            <h4 style="text-align: center; align-items: center;">I'VE NAMES OF SOME SPECIFIC MEMBERS JOKI MUJHE JADA <span style="color: {color};"> PASAND AAYE  </span>BUT WILL NOT TELL THE NAMES, LAFDE HOJAYENGE GC MAI YE SAB ACHAA NHI LAGTA  😔</h4>
-                            """, unsafe_allow_html=True)
-
-        time.sleep(10)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-        st.write(f"""
-                                    <h3 style="text-align: center; align-items: center;">VESE TO MUJHE JOIN HEE KARE 1 DIN HUA H 🤣<span style="color: {color};"></span></h3>
-                                    """, unsafe_allow_html=True)
-
-        time.sleep(4)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-        st.write(f"""
-                                            <h4 style="text-align: center; align-items: center;">And Yeah credits to all the <span style="color: {color};">ADMINS</span> and Members</h4>
-                                            """, unsafe_allow_html=True)
-
-        time.sleep(4)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-        st.columns([0.3,1,0.3])[1].write(f"""
-                                                    <h5 style="text-align: center; align-items: center;">ALSO Y'ALL MUST BE THINKING SHIVAM NIGGA WHY SO SWEET? VOTO KYA HAI SIRF <span style="color: {color};"> 1-2 DIN HUA H </span>ISLIYE I ONLY GOT TARIFS 1 WEEK BAAD SAME YAHIN AANA BHT KUCH ACHAA LIKHA HOGA 🥰</h5>
-                                                    """, unsafe_allow_html=True)
-
-        time.sleep(10)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-
-        st.columns([0.3,1,0.3])[1].write(f"""
-                                                            <h4 style="text-align: center; align-items: center;">SEE THIS IS WHERE BAKCHODI AND ROTTEN MIND TAKES YOU! <span style="color: {color};">LIKE KON</span> HEE EK WEB PAGE BANATA HOGA AISA JUST FOR THE FUN! ITS ME 😭</h4>
-                                                            """, unsafe_allow_html=True)
-
-        time.sleep(7)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-
-        st.write(f"""
-                                                                    <h4 style="text-align: center; align-items: center;">OKI END OF THE PAGE MOST PROBABLY <span style="color: {color};"></span> AB EMOJI RAIN HOGA HEHEHEHEHE</h4>
-                                                                    """, unsafe_allow_html=True)
-
-        time.sleep(7)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-
-        st.write(f"""
-                                                                            <h4 style="text-align: center; align-items: center;">THE EMOJIS ARE THE EMOTIONS WE FEEL IN THIS GC IG<span style="color: {color};"></span> ALSO WINDOWS EMOJIS SUCKS!</h4>
-                                                                            """, unsafe_allow_html=True)
-
-        time.sleep(7)
-        st.markdown("<br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-
-        st.write(f"""
-                                                                                    <h4 style="text-align: center; align-items: center;">Bhai REAL bkchodi type Rain karne wala hun pasand na aaye to reload krdena 😭😭🎀<span style="color: {color};"></span></h4>
-                                                                                    """, unsafe_allow_html=True)
-
-
-
-        time.sleep(4)
-
-        emoji = ['💀', '😂','😭', '🎀','🤣', '🫦','🥵', '😤', '🖤', '💀','💦', '😂','😭', '🎀','🤣', '🫦','🥵', '😤', '🖤']
-        for i in range(16):
-            rain(
-                emoji[i], falling_speed=0.8,
-            )
-            time.sleep(0.9)
-
-
-
-
 
     if result_search_box and '19012007' == result_search_box:
         descripE.empty()
